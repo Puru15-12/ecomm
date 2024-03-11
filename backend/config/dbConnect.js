57d8f09efabf01b8a8b7b00d2cf43804de89ff70
aayush078 <raghuwanshiaayush712@gmail.com>
2024-03-12 00:45:12 +0530

    exact

--- backend/config/dbConnect.js
+++ backend/config/dbConnect.js
@@ -1,12 +1,18 @@
 import mongoose from "mongoose";
 
-export const  connectDatabase = () => {
-    let DB_URI = "";
-    if(process.env.NODE_ENV === 'DEVELOPMENT') DB_URI = process.env.DB_LOCAL_URI;
-    if(process.env.NODE_ENV === 'PRODUCTION') DB_URI = process.env.DB_URI;
-    mongoose.connect(DB_URI).then((con) => {
-        console.log(
-          `MongoDB Database connected with HOST: ${con?.connection?.host}`
-        );
-    });
+import dotenv from 'dotenv';
+
+dotenv.config();
+export const connectDatabase = () => {
+//  const DB_URI = process.env.DB_LOCAL_URI ;
+//  if (process.env.NODE_ENV === 'DEVELOPMENT') DB_URI = process.env.DB_LOCAL_URI;
+// if (process.env.NODE_ENV === 'PRODUCTION') DB_URI = process.env.DB_URI;
+console.log(process.env.DB_LOCAL_URI);
+  mongoose.connect(process.env.DB_LOCAL_URI).then((con) => {
+    console.log(
+      `MongoDB Database connected with HOST: ${con?.connection?.host}`
+    );
+  }).catch ((error) => {
+    console.log(`can not connect to database, ${error}`);
+  });
 };
