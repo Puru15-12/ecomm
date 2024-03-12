5c846cdcfdaa372084bebb0a091517974cc9631c
Puru15-12 <singhpuru36@gmail.com>
2024-03-13 01:29:18 +0530

    Login completed with tokens stored in cookies

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,5 +1,7 @@
 import mongoose from "mongoose";
-import bcrypt from 'bcryptjs';
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
 const userSchema = new mongoose.Schema(
   {
     name: {
@@ -17,17 +19,33 @@ const userSchema = new mongoose.Schema(
       required: [true, "Please enter your password"],
       minLength: [6, "Your password must be longer than 6 characters"],
       select: false,
-    },
+    }
   },
   { timestamps: true }
 );
 
-userSchema.pre("save", async function(next){
-  if(!this.isModified("password")){
+// Encrypting password before saving the user
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
     next();
   }
 
   this.password = await bcrypt.hash(this.password, 10);
 });
+
+// Return JWT Token
+userSchema.methods.getJwtToken = function () {
+  return jwt.sign({ id : this._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_TIME,
+  });
+};
+
+// Compare user password
+
+userSchema.methods.comparePassword = async function (password_entered)
+{
+  return await bcrypt.compare(password_entered ,this.password);
+};
+
 
 export default mongoose.model("User", userSchema);
