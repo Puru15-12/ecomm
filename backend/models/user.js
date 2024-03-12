02fb400f412cc55b8a1e782ac541c403f35ac798
Piyush20-11 <piyush275202@gmail.com>
2024-03-12 17:45:47 +0530

    Register Post added

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,8 +1,5 @@
 import mongoose from "mongoose";
-import bcrypt from "bcryptjs";
-import jwt from "jsonwebtoken";
-import crypto from "crypto";
-
+import bcrypt from 'bcryptjs';
 const userSchema = new mongoose.Schema(
   {
     name: {
@@ -21,18 +18,16 @@ const userSchema = new mongoose.Schema(
       minLength: [6, "Your password must be longer than 6 characters"],
       select: false,
     },
-    avatar: {
-      public_id: String,
-      url: String,
-    },
-    role: {
-      type: String,
-      default: "user",
-    },
-    resetPasswordToken: String,
-    resetPasswordExpire: Date,
   },
   { timestamps: true }
 );
+
+userSchema.pre("save", async function(next){
+  if(!this.isModified("password")){
+    next();
+  }
+
+  this.password = await bcrypt.hash(this.password, 10);
+});
 
 export default mongoose.model("User", userSchema);
