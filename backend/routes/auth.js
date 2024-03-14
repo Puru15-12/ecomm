f49db04b3d3fb0db5b5a32d768027afa785df707
aayush078 <raghuwanshiaayush712@gmail.com>
2024-03-14 15:53:20 +0530

    Reset Password implemented

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,6 +1,18 @@
 import express from "express";
 const router = express.Router();
-import { registerUser ,loginUser, logout, getUserProfile, updatePassword, updateProfile, getAllUsers, getUserDetails, updateUser, deleteUser} from "../controllers/authControllers.js";
+import { registerUser ,
+      loginUser,
+      logout,
+      forgotPassword,
+      resetPassword,
+      getUserProfile,
+      updatePassword,
+      updateProfile,
+      allUsers,
+      getUserDetails,
+      updateUser,
+      deleteUser}
+      from "../controllers/authControllers.js";
 import { isAuthenticatedUser ,authorizeRoles} from "../middlewares/auth.js";
 
 
@@ -8,16 +20,23 @@ router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
 router.route("/logout").get(logout);
 
+router.route("/password/forgot").post(forgotPassword);
+router.route("/password/reset/:token").put(resetPassword);
+
+
 router.route("/me").get( isAuthenticatedUser ,getUserProfile);
 router.route("/me/update").put( isAuthenticatedUser ,updateProfile);
 
-router.route("/update").put(isAuthenticatedUser , updatePassword);
+router.route("/password/update").put(isAuthenticatedUser , updatePassword);
 
-router.route("/admin/allusers").get( isAuthenticatedUser ,authorizeRoles("admin") , getAllUsers);
-router.route("/admin/allusers/:id")
-                .get( isAuthenticatedUser ,authorizeRoles("admin") , getUserDetails)
-                .put(isAuthenticatedUser, authorizeRoles("admin"), updateUser)
-                .delete(isAuthenticatedUser, authorizeRoles("admin"), deleteUser);
+router
+      .route("/admin/users")
+      .get( isAuthenticatedUser ,authorizeRoles("admin") , allUsers);
+router
+      .route("/admin/users/:id")
+      .get( isAuthenticatedUser ,authorizeRoles("admin") , getUserDetails)
+      .put(isAuthenticatedUser, authorizeRoles("admin"), updateUser)
+      .delete(isAuthenticatedUser, authorizeRoles("admin"), deleteUser);
 
 
 export default router;
