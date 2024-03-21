d54c80f8d512af3cee20e25e2951d27a586d18ea
aayush078 <raghuwanshiaayush712@gmail.com>
2024-03-22 01:23:40 +0530

    Search Products Component

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -11,8 +11,9 @@ const Home = () => {
 
   let [searchParams] = useSearchParams();
   const page = searchParams.get("page") || 1;
+  const keyword = searchParams.get("keyword") || "";
 
-  const params = {page};
+  const params = {page , keyword};
 
   const { data, isLoading, error, isError } = useGetProductsQuery(params);
 
@@ -22,21 +23,29 @@ const Home = () => {
     }
   }, [isError]);
 
+  const columnSize = keyword ? 4 : 3;
+
   if (isLoading) return <Loader />;
   
   return (
     <>
       <MetaData title={"Buy Best Products Online"} />
       <div className="row">
-      <div className="col-6 col-md-12">
+        {keyword && (
+          <div className="coll-6 col-md-3 mt-5">
+            <p>FILTERS</p>
+           </div> 
+        )}
+      <div className={keyword ? "col-6 col-md-9" : "col-6 col-md-12"}>
           <h1 id="products_heading" className="text-secondary">
-            Latest Products
+            {keyword ? `${data?.products?.length} Products found with keyword : ${keyword}`
+            : "Latest Products" }
           </h1>
 
           <section id="products" className="mt-5">
             <div className="row">
                 {data?.products?.map((product) =>(
-                  <ProductItem product={product} />
+                  <ProductItem product={product} columnSize={columnSize} />
                 ))}
             </div>
           </section>
