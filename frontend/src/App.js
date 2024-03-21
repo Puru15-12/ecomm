f047f579a26c1977190ba972c22fc5be831441ce
Piyush20-11 <piyush275202@gmail.com>
2024-03-21 09:46:03 +0530

    Integrate Login and Register User using Redux

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,8 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Home from "./components/Home.jsx";
+import Login from "./components/auth/login.jsx"
+import Register from "./components/auth/register.jsx"
 
 import Footer from "./components/layout/Footer";
 import Header from "./components/layout/Header";
@@ -16,6 +18,8 @@ function App() {
         <div className="container">
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
           </Routes>
         </div>
 
