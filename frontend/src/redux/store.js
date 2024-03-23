b525555cf81b82207df5ae7f03dd56ddf2481fad
Piyush20-11 <piyush275202@gmail.com>
2024-03-23 15:58:48 +0530

    Load logged in user state, Show user in header and Logout User

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -2,22 +2,25 @@
 import{configureStore} from"@reduxjs/toolkit";
 import { productApi } from "./api/productsApi";
 
-export const store = configureStore({
-    reducer:{
-        [productApi.reducerPath]  : productApi.reducer,
-    },
-    middleware : (getDefaultMiddleware) => 
-    getDefaultMiddleware().concat([productApi.middleware]),
-}); 
+import userReducer from "./features/userSlice";
 
 import { authApi } from "./api/authApi";
 
+import { userApi } from "./api/userApi";
+
 export const store = configureStore({
-  reducer: {
-    [authApi.reducerPath]: authApi.reducer
-  },
-  middleware: (getDefaultMiddleware) =>
+
+    reducer:{
+      auth: userReducer,
+      [productApi.reducerPath]  : productApi.reducer,
+      [authApi.reducerPath]: authApi.reducer,
+      [userApi.reducerPath]: userApi.reducer
+    },
+    middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat([
-      authApi.middleware
-    ]),
-});
+      productApi.middleware,
+      authApi.middleware,
+      userApi.middleware
+    ])
+}); 
+
